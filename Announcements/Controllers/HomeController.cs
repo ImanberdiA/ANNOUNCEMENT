6d0822d62fa16d05690c1b12d167c72a659e6340
ImanberdiA <31826437+ImanberdiA@users.noreply.github.com>
2019-12-09 00:32:18 +0600

    Get All Anns

--- Announcements/Controllers/HomeController.cs
+++ Announcements/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Announcements.Settings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,11 @@ namespace Announcements.Controllers
 {
     public class HomeController : Controller
     {
+        AppIdentityDbContext db = new AppIdentityDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Announs.ToList());
         }
     }
 }
